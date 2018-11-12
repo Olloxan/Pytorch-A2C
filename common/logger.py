75ec8543b9d85fc7c4af69c38d83e0604d83916e
Olloxan <axe999@gmx.de>
2018-11-12 11:38:53 +0100

    addded in some print statements to store functions of state_dicts and data

--- common/logger.py
+++ common/logger.py
@@ -1,6 +1,7 @@
 import io
 import pickle
 import os
+import torch
 
 class Logger():
     
@@ -12,6 +13,11 @@ class Logger():
             directoryPath += "/"
         directoryPath += filename
         self.dump(data=data, filename=directoryPath)
+        print("{0} saved".format(filename))
+
+    def log_state_dict(self, state_dict, path, name=""):
+        torch.save(state_dict, path)
+        print("Model {0} saved".format(name))
 
     def dump(self, data, filename):
         with open(filename, "wb") as f:
@@ -20,6 +26,11 @@ class Logger():
     def load(self, filename):
         with open(filename, 'rb') as f:
             data = pickle.load(f)
+        return data
+
+    def loadwithoutfirstelem(self, filename):
+        data = self.load(filename)
+        del data[0]
         return data
 
     def printDayFormat(self, message, seconds):
